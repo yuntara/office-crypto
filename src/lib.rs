7f447ed9e609b4fa3935ed9007ec8e59b6c56f7c
yuntara <yuntara@mail.yuntara.net>
2024-05-21 10:51:20 +0900

    test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,20 @@
+use wasm_bindgen::{prelude::*, JsValue};
+
 pub mod crypto;
 pub mod errors;
+
+#[wasm_bindgen]
+pub fn decrypt(data: JsValue, password: &str) -> Result<Vec<u8>, JsValue> {
+    let array = js_sys::Uint8Array::new(&data);
+    let bytes: Vec<u8> = array.to_vec();
+
+    let decrypted_bytes = match crate::crypto::decrypt_from_bytes(bytes, password) {
+        Ok(bytes) => bytes,
+        Err(e) => return Err(JsValue::from_str(&e.to_string())),
+    };
+
+    Ok(decrypted_bytes)
+}
 
 #[cfg(test)]
 mod test {
