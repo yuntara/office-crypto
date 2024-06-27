8d63cd68e0c19ce04dcb4211a6fe8ff6985fa604
Kenta Kudo <yd37574@gmail.com>
2024-06-27 09:34:08 +0300

    refactor(wasm): iface

--- office-crypto-wasm/src/lib.rs
+++ office-crypto-wasm/src/lib.rs
@@ -3,11 +3,11 @@ mod utils;
 use wasm_bindgen::{prelude::*, JsValue};
 
 #[wasm_bindgen]
-pub fn decrypt(data: JsValue, password: &str) -> Result<Vec<u8>, JsValue> {
-    let array = js_sys::Uint8Array::new(&data);
-    let bytes: Vec<u8> = array.to_vec();
+pub fn decrypt(buffer: &[u8], password: &str) -> Result<Vec<u8>, JsValue> {
+    // let array = js_sys::Uint8Array::new(&data);
+    // let bytes: Vec<u8> = array.to_vec();
 
-    let decrypted_bytes = match office_crypto_rs::decrypt_from_bytes(bytes, password) {
+    let decrypted_bytes = match office_crypto_rs::decrypt_from_bytes(buffer, password) {
         Ok(bytes) => bytes,
         Err(e) => return Err(JsValue::from_str(&e.to_string())),
     };
